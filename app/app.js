4f037c4b563905bb59e2386e92e2ea31be7b4043
hkiichi <iris.kikuchi@gmail.com>
2019-07-16 17:10:39 +0200

    source and app folder added

---
+++ app/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/app';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+ReactDom.render(<App />, container);
+
