955e51471043e4332ab8ad214037611ee4f2aed8
hkiichi <iris.kikuchi@gmail.com>
2019-07-23 15:34:42 +0200

    configured webpack

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 });
 
